abd0e40b286fe8ffa5cdfa24f4382553e5d5b892
Paulo Gomes <paulomjg@hotmail.com>
2025-01-22 14:29:44 +0900

    c

--- 18. Dom and JS/index.js
+++ 18. Dom and JS/index.js
@@ -1,47 +1,61 @@
-var buttons = document.querySelectorAll("button")
+document.addEventListener("click", function (event) {
+    makeSound(event.target.innerHTML)
+    buttonAnimation(event.target.innerHTML)
 
-for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function () { 
+})
 
-        var buttonInnerHTML = this.innerHTML
+document.addEventListener("keydown", function (event) {
+    makeSound(event.key)
+    buttonAnimation(event.key)
 
-        switch (buttonInnerHTML) {
-            case "w":
-                var audio = new Audio('./sounds/tom-1.mp3')
-                audio.play()
-                break
+})
+    
+function makeSound (base) {
+    switch (base) {
+        case "w":
+            var audio = new Audio('./sounds/tom-1.mp3')
+            audio.play()
+            break
 
-            case "a":
-                var audio = new Audio('./sounds/tom-2.mp3')
-                audio.play()
-                break
+        case "a":
+            var audio = new Audio('./sounds/tom-2.mp3')
+            audio.play()
+            break
 
-            case "s":
-                var audio = new Audio('./sounds/tom-3.mp3')
-                audio.play()
-                break
+        case "s":
+            var audio = new Audio('./sounds/tom-3.mp3')
+            audio.play()
+            break
 
-            case "d":
-                var audio = new Audio('./sounds/tom-4.mp3')
-                audio.play()
-                break
-            
-            case "j":
-                var audio = new Audio('./sounds/snare.mp3')
-                audio.play()
-                break
+        case "d":
+            var audio = new Audio('./sounds/tom-4.mp3')
+            audio.play()
+            break
+        
+        case "j":
+            var audio = new Audio('./sounds/snare.mp3')
+            audio.play()
+            break
 
-            case "k":
-                var audio = new Audio('./sounds/crash.mp3')
-                audio.play()
-                break
+        case "k":
+            var audio = new Audio('./sounds/crash.mp3')
+            audio.play()
+            break
 
-            case "l":
-                var audio = new Audio('./sounds/kick-bass.mp3')
-                audio.play()
-                break
-        }
-
-    })
+        case "l":
+            var audio = new Audio('./sounds/kick-bass.mp3')
+            audio.play()
+            break
+    }
 }
 
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey)
+    activeButton.classList.add("pressed")
+    setTimeout(() => {
+        activeButton.classList.remove("pressed"); 
+      }, 150); 
+
+
+}
