e79b4c829566f1c19dac062b638bef5e9cbf4425
Paulo Gomes <paulomjg@hotmail.com>
2025-03-11 19:07:55 +0900

    c

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,10 @@
+// import generateName from "sillyname"
+// var sillyName = generateName()
+
+// console.log(`My name is ${sillyName}.`)
+
+
+import { randomSuperhero } from "superheroes"
+var name = randomSuperhero()
+
+console.log(`My super heroe name is ${name}.`)
